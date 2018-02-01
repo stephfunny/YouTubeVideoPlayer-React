f3c1b187fdb21ce83ee1049af9b57d880b98536c
stephfunny <contactstephanief@gmail.com>
2018-01-31 20:40:09 -0800

    compiler babel wasn't watching, fixed

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -10,12 +10,12 @@ var VideoPlayer = (props) => (
   </div>
 );
 
-// PropTypes tell other developers what `props` a component expects
-// Warnings will be shown in the console when the defined rules are violated
-VideoPlayer.propTypes = {
-  video: React.PropTypes.object.isRequired
-};
+// // PropTypes tell other developers what `props` a component expects
+// // Warnings will be shown in the console when the defined rules are violated
+// VideoPlayer.propTypes = {
+//   video: React.PropTypes.object.isRequired
+// };
 
-// In the ES6 spec, files are "modules" and do not share a top-level scope
-// `var` declarations will only exist globally where explicitly defined
-window.VideoPlayer = VideoPlayer;
+// // In the ES6 spec, files are "modules" and do not share a top-level scope
+// // `var` declarations will only exist globally where explicitly defined
+// window.VideoPlayer = VideoPlayer;
